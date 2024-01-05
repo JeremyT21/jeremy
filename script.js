eb8503e97a952cc97243357253b170da8b41a5de
JeremyT21 <thummeljeremy@gmail.com>
2024-01-04 23:31:30 -0500

    updatedHTML

---
+++ script.js
@@ -0,0 +1,42 @@
+//Gets initial value of certain variables. (e.g. original logo sizes)
+var o_logoGithubSize = document.getElementById('githubLogo').clientWidth;
+
+//Website loads being properly displayed.
+var topBar = document.getElementById('header');
+var topBarHeight = topBar.clientHeight;
+var extraSpace = document.getElementById('belowMargin');
+extraSpace.style.height = (topBarHeight / 8) + "%";
+console.log(topBarHeight);
+
+//Every half a second website resizes the relativity of page elements appropriately in reference to the fixed position of the top bar.
+setInterval(function()
+{
+	topBar = document.getElementById('header');
+    topBarHeight = topBar.clientHeight;
+    extraSpace = document.getElementById('belowMargin');
+    extraSpace.style.height = (topBarHeight + 10) + "px";
+    console.log(topBarHeight);
+},500);
+
+//Every half a second website resizes the relativity of page elements appropriately in reference to the yellow rectangluar background.
+setInterval(function()
+{
+    var pdf = document.getElementById('pdf1');
+    var pdfH = pdf.clientHeight;
+	var _rBG = document.getElementById('rBG');
+    var rBGHeight = _rBG.clientHeight;
+    
+    var extraSpace2 = document.getElementById('belowMargin2');
+    extraSpace2.style.paddingBottom = (rBGHeight + pdfH) + "px";
+    extraSpace2.style.marginTop = -rBGHeight + "px";
+},500);
+
+//Keeps track of scrolling on the website and when to fade certain elements in.
+document.addEventListener('scroll', function(){
+    if(window.scrollY >= 100)
+    {
+        var pdfObject = document.getElementById('pdf1');
+        pdfObject.style.animation = "fade 2s";
+        pdfObject.style.opacity = 1;
+    }
+});
