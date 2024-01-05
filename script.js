1276cadc2868d86c69ebd942c6805a9c16850889
JeremyT21 <thummeljeremy@gmail.com>
2024-01-05 02:07:52 -0500

    addedProjects

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ setInterval(function()
     console.log(topBarHeight);
 },500);
 
-//Every half a second website resizes the relativity of page elements appropriately in reference to the yellow rectangluar background.
+//Every half a second website resizes the relativity of page elements appropriately in reference to the rectangular background.
 setInterval(function()
 {
     var pdf = document.getElementById('pdf1');
@@ -28,7 +28,7 @@ setInterval(function()
     
     var extraSpace2 = document.getElementById('belowMargin2');
     extraSpace2.style.paddingBottom = (rBGHeight + pdfH) + "px";
-    extraSpace2.style.marginTop = -rBGHeight + "px";
+    extraSpace2.style.marginTop = (-rBGHeight-400) + "px";
 },500);
 
 //Keeps track of scrolling on the website and when to fade certain elements in.
