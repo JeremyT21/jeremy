bee53398a031d0a63861581f270041b9df446731
JeremyT21 <thummeljeremy@gmail.com>
2024-01-05 11:40:16 -0500

    subtitleFix

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//Gets initial value of certain variables. (e.g. original logo sizes)
-var o_logoGithubSize = document.getElementById('githubLogo').clientWidth;
-
 //Website loads being properly displayed.
 var topBar = document.getElementById('header');
 var topBarHeight = topBar.clientHeight;
 var extraSpace = document.getElementById('belowMargin');
 extraSpace.style.height = (topBarHeight / 8) + "%";
-console.log(topBarHeight);
+//console.log(topBarHeight);
 
 //Every half a second website resizes the relativity of page elements appropriately in reference to the fixed position of the top bar.
 setInterval(function()
@@ -15,12 +12,38 @@ setInterval(function()
     topBarHeight = topBar.clientHeight;
     extraSpace = document.getElementById('belowMargin');
     extraSpace.style.height = (topBarHeight + 10) + "px";
-    console.log(topBarHeight);
+    //console.log(topBarHeight);
+},500);
+
+//Every half a second website resizes the relativity of page elements appropriately in reference to the header bar's text.
+setInterval(function()
+{
+	var nameHeader = document.getElementById('name');
+    nameHeader.style.fontSize = document.getElementById('githubLogo').clientHeight + "px";
+
+    //Below code gets the font-size of the Jeremy Thummel header, and changes the subtitle font-size relatively.
+    var styleAttributes = window.getComputedStyle(nameHeader);
+    nameFontSize = styleAttributes.getPropertyValue('font-size');
+    var fontSizeString = nameFontSize.replace('px', '');
+    var fontSizeNum = parseInt(fontSizeString);
+
+    var sub = document.getElementById('subtitleHeader');
+
+    if(fontSizeNum > 25)
+        sub.style.fontSize = 25 + "px";
+
+    else if(fontSizeNum > 20)
+        sub.style.fontSize = (fontSizeNum - 10) + "px";
+
+    else
+        sub.style.fontSize = 10 + "px";//The px value must be the number in the if statement - 10.
+
+    //console.log(fontSizeNum);
 },500);
 
 //Keeps track of scrolling on the website and when to fade certain elements in.
 document.addEventListener('scroll', function(){
-    if(window.scrollY >= 100)
+    if(window.scrollY >= 100)//Fades in the pdf.
     {
         var pdfObject = document.getElementById('pdf1');
         pdfObject.style.animation = "fade 2s";
